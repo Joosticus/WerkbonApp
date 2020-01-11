78048464c638cbb8964400b75ffda0327506ba53
Rienk <rienk_goot@hotmail.com>
2020-01-11 22:35:29 +0100

    Hide JS files

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,14 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(function () {
+    var canvas = document.querySelector('#signature');
+    var pad = new SignaturePad(canvas);
 
-// Write your Javascript code.
+    var canvasSecond = document.querySelector('#signatureSecond');
+    var padSecond = new SignaturePad(canvasSecond);
+    $('#accept').click(function () {
+        var data = pad.toDataURL();
+        $('#SignatureDataUrl').val(data);
+        var dataSecond = padSecond.toDataURL();
+        $('#SignatureDataUrlSecond').val(dataSecond);
+        pad.off();
+    });
+});
